638796e9ce0396fd62f04b21fc17d69078acf9c4
Sangam Thapa <swngam26@gmail.com>
2025-05-07 00:41:38 +0545

    Admin Panel is done, Admin pages are connected now to add other admin pages

---
+++ Kitabh_Chautari/Utilities/NavigationManagerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Kitabh_Chautari.Utilities
+{
+    public static class NavigationManagerExtensions
+    {
+        public static string GetQueryString(this NavigationManager navigationManager, string key)
+        {
+            var uri = navigationManager.ToAbsoluteUri(navigationManager.Uri);
+            var queryStrings = System.Web.HttpUtility.ParseQueryString(uri.Query);
+            return queryStrings[key];
+        }
+    }
+}
